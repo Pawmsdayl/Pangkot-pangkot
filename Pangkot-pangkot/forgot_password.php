f6a48d3155b519a76e993bba6474f3318c70b64f
Palmsdale Kevin Dagohoy Cordero <99542164+Pawmsdayl@users.noreply.github.com>
2023-06-04 14:51:55 +0800

    Update forgot_password.php

--- Pangkot-pangkot/forgot_password.php
+++ Pangkot-pangkot/forgot_password.php
@@ -2,23 +2,21 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="forgot-password.css">
     <title>Forgot Password</title>
+
+    <?php
+        include 'dbConnector.php';
+        include 'commonHead.php'
+    ?>
+    
+    <link rel="stylesheet" href="forgot-password.css">
 </head>
 
-<body>
-    <header>
-        <h1>Logo</h1>
-        <nav>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+<body>    
+    <?php
+        include 'header.php';
+        include 'ads.php';
+    ?>
 
     <div class="forgot-password-container">
         <h2>Forgot Password</h2>
@@ -36,14 +34,9 @@
         </form>
     </div>
 
-    <footer>
-        <div id="footerRuler"></div>
-        <div class="footerButton" id="toAbout">About</div>
-        <div class="footerButton" id="toFaqs">FAQs</div>
-        <p class="copyright">
-            &copy; 2023 Your Company. All rights reserved.
-        </p>
-    </footer>
+    <?php
+        include 'footer.php';
+    ?>
 </body>
 
 </html>
