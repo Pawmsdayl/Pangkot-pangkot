b2641f145b7f8ca9071dc04224851a7f55d31176
Pawmsdayl <99542164+Pawmsdayl@users.noreply.github.com>
2023-06-08 11:38:12 +0800

    edit name function

--- Pangkot-pangkot/dbSettingsName.php
+++ Pangkot-pangkot/dbSettingsName.php
@@ -1,5 +1,27 @@
 <?php
-    include 'dbConnector.php';
+include 'dbConnector.php';
 
+$account_id = $_SESSION['account_id'];
+$new_username = validateInput($_POST['new_username']);
+
+$sql = "UPDATE account
+        SET username = '$new_username'
+        WHERE account_id = '$account_id'
+;";
+$result = $conn->query($sql);
+$_SESSION['username'] = $new_username;
+header("Location: settings.php?success=Username changed. Welcome, $new_username!");
+exit(); 
     
+function validateInput($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+
+    if (empty($data) || $data == null || $data == "") {
+        header("Location: settings.php?error=Username cannot be empty.");
+        exit();
+    }
+    return $data;
+}
 ?>
