3f29904b6dc9a77c2ab4ed41f78f1ffdfbb3096e
Pawmsdayl <99542164+Pawmsdayl@users.noreply.github.com>
2023-06-08 10:46:06 +0800

    flashcard transition

--- Pangkot-pangkot/quiz.php
+++ Pangkot-pangkot/quiz.php
@@ -1,72 +1,293 @@
+<?php
+    include 'dbConnector.php';
+    $quiz_id = $_GET['quiz_id'];
+    $account_id = $_SESSION['account_id'];
+
+    $sql = "SELECT *
+            FROM quiz
+            WHERE quiz_id = $quiz_id
+    ;";
+    $result = $conn->query($sql);
+
+    $row = $result->fetch_assoc();
+    $group_id = $row['group_id'];
+    $quiz_account_id = $row['account_id'];
+    $quiz_name = $row['quiz_name'];
+    $quiz_description = $row['quiz_description'];
+    $creation_date = $row['creation_date'];
+    $timer = $row['timer'];
+    $timer_minutes = floor($timer / 60);
+    $timer_seconds = $timer % 60;
+    $total_timer = $timer_minutes. ":". $timer_seconds;
+
+    switch($group_id) {
+    case 1:         // Public
+        $quit_href = "index.php?success=You have quit the quiz.";
+        break;
+
+    case 2:         // Private
+        $quit_href = "private.php?success=You have quit the quiz.";
+        if ($account_id != $quiz_account_id) {
+            header("Location: index.php?error=You are not authorized to view this quiz.");
+            exit();
+        }
+        break;
+
+    default:        // Group
+        $quit_href = "group.php?group_id=". $group_id. "&success=You have quit the quiz.";
+        $sql = "SELECT *
+                FROM membership
+                WHERE group_id = $group_id
+                AND account_id = $account_id
+        ;";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows == 0) {
+            header("Location: index.php?error=You are not authorized to view this quiz.");
+            exit();
+        }
+        break;
+    }
+
+    $sql = "SELECT *
+            FROM flashcard
+            WHERE quiz_id = $quiz_id
+    ;";
+    $allFlashcards = $conn->query($sql);
+
+    $sql = " SELECT MAX(flashcard_number) AS card_count
+             FROM flashcard
+             WHERE quiz_id = $quiz_id
+    ;";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    $card_count = $row['card_count'];
+    $total_card_count = $card_count;
+
+    $sql = "SELECT MAX(date_taken) AS last_trial_date, AVG(time_took) AS ave_time, MIN(time_took) AS best_time
+            FROM record
+            WHERE quiz_id = $quiz_id
+            AND account_id = $account_id
+    ;";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    $last_trial_date = (isset($row['last_trial_date'])) ? $row['last_trial_date'] : "yyyy/mm/dd";
+    $ave_minutes = floor($row['ave_time'] / 60);
+    $ave_minutes = str_pad($ave_minutes, 2, "0", STR_PAD_LEFT);
+    $ave_seconds = $row['ave_time'] % 60;
+    $ave_seconds = str_pad($ave_seconds, 2, "0", STR_PAD_LEFT);
+    $ave_time = $ave_minutes. ":". $ave_seconds;
+    $best_minutes = floor($row['best_time'] / 60);
+    $best_minutes = str_pad($best_minutes, 2, "0", STR_PAD_LEFT);
+    $best_seconds = $row['best_time'] % 60;
+    $best_seconds = str_pad($best_seconds, 2, "0", STR_PAD_LEFT);
+    $best_time = $best_minutes. ":". $best_seconds;
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Answer Quiz</title>
-
+    <title>Flashcard Quiz</title>
     <?php
-        include 'dbConnector.php';
         include 'commonHead.php';
     ?>
-
-    <link rel="stylesheet" type="text/css" href="style/quiz.css">
+    <link rel="stylesheet" type="text/css" href="style/answer.css">
 </head>
+
 <body>
     <?php
         include 'header.php';
     ?>
 
-    <div class="quit-and-info">
-        <button class="quit-button">Quit</button>
-        <div class="quiz-info">
-            <span>Cards Left: --:--</span>
-            <span>Time Left: --:--</span>
+    <div class="quiz-info">
+        <a href= "<?php echo $quit_href; ?>" >
+            <button class="quit-button" onclick="quitQuiz()">Quit</button>
+        </a>
+        <div class="info-right">
+            <div class="cards-left">Cards Left: <span id="cardsLeft"><?php echo $card_count; ?></span></div>
+            <div class="time-left">Time Left: <span id="timeLeft"> <?php echo $timer_minutes. ":". $timer_seconds; ?> </span></div>
         </div>
     </div>
-
-    <div class="flashcard-container">
-        <div class = "flashcard">
-            <table class="flashcard-info">
-                <tr>
-                    <th class="card-title" colspan="2">Flashcard Title</th>
-                </tr>
-                <tr>
-                    <td colspan="2">Timer: 00:00</td>
-                </tr>
-                <tr>
-                    <td colspan="2">Card: 1 of 10</td>
-                </tr>
-                <tr>
-                    <td>Description:</td>
-                    <td>
-                        <ul>
-                            <li>Date Created:</li>
-                            <li>Your Last Answer:</li>
-                            <li>Your Average Score:</li>
-                            <li>Your Average Time:</li>
-                            <li>Total Average Score</li>
-                            <li>Total Average Time:</li>
-                            <li>Your Best Score:</li>
-                            <li>Your Best Time:</li>
-                        </ul>
-                    </td>
-                </tr>
-            </table>
-        </div>
-        <div class="next-back-buttons">
-            <button id="back-button">&#8249;</button>
-            <button id="next-button" onclick="nextCard()">&#8250;</button>
-        </div>
-        <div class="start-quiz-container">
+    <div class="flashcard">
+        <table class="flashcard-info">
+            <tr>
+                <th class="card-title" colspan="2"> <?php echo $quiz_name; ?></th>
+            </tr>
+            <tr>
+                <td colspan="2">Timer: <span id="timer"><?php echo $total_timer ?></span></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <label for="cards-number">Cards: <span id="card-count"><?php echo $total_card_count; ?></span></label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="description"></label>
+                    <textarea id="description" name="description" placeholder="-Edit Description Here-"></textarea>
+                </td>
+                <td>
+                    <ul>
+                        <li>Date Created: <?php echo $creation_date; ?></li>
+                        <li>Your Last Answer:  <?php echo $last_trial_date; ?></li>
+                        <li>Your Average Time:  <?php echo $ave_time; ?></li>
+                        <li>Your Best Time:  <?php echo $best_time; ?></li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+        <div class="start-button-container">
             <button class="start-quiz-button">Start Quiz</button>
         </div>
-        
     </div>
-    <div class="flashcard-behind"></div>
+    <div id="card-container" class="card-container">
+    <!-- Cards will be dynamically added here -->
+    </div>
+    <div class="card-buttons">
+        <button class="back-button" disabled>&#8249;</button>
+        <button class="next-button" disabled>&#8250;</button>
+    </div>
 
-    <?php
-        include 'footer.php';
-    ?>
+    <script>
+        var startButton = document.querySelector('.start-quiz-button');
+        startButton.addEventListener('click', startQuiz);
+
+        var cardContainer = document.getElementById('card-container');
+        var maxCardCount = <?php echo $total_card_count ?> ;
+        console.log("maxCardCount: " + maxCardCount);
+        var currentCardIndex = 0;
+
+        var cards = [
+        ];
+        <?php 
+            while ($row = $allFlashcards->fetch_assoc()) {
+                $question = $row['question'];
+                $answer = $row['answer'];
+                $flashcard_number = $row['flashcard_number'] - 1;
+                echo "cards['$flashcard_number'] = { question: '$question', answer: '$answer' };";
+            }
+        ?>
+        console.log(cards);
+
+        function createCard(question, answer) {
+        var card = document.createElement('div');
+        card.className = 'card';
+
+        var cardFront = document.createElement('div');
+        cardFront.className = 'card-front';
+        cardFront.innerHTML = `<h3>${question}</h3>`;
+
+        var cardBack = document.createElement('div');
+        cardBack.className = 'card-back';
+        cardBack.innerHTML = `<h3>${answer}</h3>`;
+
+        card.appendChild(cardFront);
+        card.appendChild(cardBack);
+
+        // Add event listener to flip the card on click
+        card.addEventListener('click', function () {
+            card.classList.toggle('flipped');
+        });
+
+        return card;
+        }
+
+        function startQuiz() {
+            // Hide the flashcard info
+            var flashcardInfo = document.querySelector('.flashcard-info');
+            flashcardInfo.style.display = 'none';
+
+            // Clear the card container
+            cardContainer.innerHTML = '';
+
+            // Reset the current card index
+            currentCardIndex = 0;
+
+            // Shuffle the cards array
+            shuffleArray(cards);
+
+            // Enable the Next and Back buttons
+            var nextButton = document.querySelector('.next-button');
+            nextButton.disabled = false;
+
+            var backButton = document.querySelector('.back-button');
+            backButton.disabled = false;
+
+            // Display the first card
+            displayCard(currentCardIndex);
+            document.getElementById("cardsLeft").innerHTML = maxCardCount - currentCardIndex - 1;
+
+        }
+
+        function displayCard(cardIndex) {
+        // Check if the card index is within the valid range
+            if (cardIndex >= 0 && cardIndex < cards.length) {
+                // Get the current card from the cards array
+                var currentCard = cards[cardIndex];
+
+                // Create a card element
+                var card = createCard(currentCard.question, currentCard.answer);
+
+                // Clear the card container
+                cardContainer.innerHTML = '';
+
+                // Append the card to the card container
+                cardContainer.appendChild(card);
+
+                // Disable the Back button if it's the first card
+                var backButton = document.querySelector('.back-button');
+                backButton.disabled = cardIndex === 0;
+
+                // Disable the Next button if it's the last card
+                var nextButton = document.querySelector('.next-button');
+                nextButton.disabled = cardIndex === cards.length - 1;
+            }
+        }
+
+        function showNextCard() {
+            if (currentCardIndex < maxCardCount - 1) {
+                // Increment the current card index
+                currentCardIndex++;
+
+                // Display the next card
+                displayCard(currentCardIndex);
+                document.getElementById("cardsLeft").innerHTML = maxCardCount - currentCardIndex - 1;
+            }
+        }
+
+        function showPreviousCard() {
+            if (currentCardIndex > 0) {
+                // Decrement the current card index
+                currentCardIndex--;
+
+                // Display the previous card
+                displayCard(currentCardIndex);
+                document.getElementById("cardsLeft").innerHTML = maxCardCount - currentCardIndex - 1;
+            }
+        }
+
+        // Attach event listeners to the next and back buttons
+        var nextButton = document.querySelector('.next-button');
+        nextButton.addEventListener('click', showNextCard);
+
+        var backButton = document.querySelector('.back-button');
+        backButton.addEventListener('click', showPreviousCard);
+
+        // Disable the Next and Back buttons initially
+        nextButton.disabled = true;
+        backButton.disabled = true;
+
+        // Function to shuffle the cards array
+        function shuffleArray(array) {
+            for (var i = array.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    </script>
 </body>
 </html>
